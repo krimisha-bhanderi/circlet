07ca149fe436daa1be88991365d46c778d8f49cd
krimisha-bhanderi <krimisha025@gmail.com>
2023-05-18 12:14:10 +0100

    c program

---
+++ CIRCLET1.C
@@ -0,0 +1,15 @@
+#include<conio.h>
+main()
+{
+  int i,j,k,m;
+  clrscr();
+   for (i=41;i<=45;i++)
+   {
+   for(j=41;j<=i;j++)
+  {
+  printf("%d\t",j);
+  }
+  printf("\n");
+  }
+getch();
+}
---
+++ CIRCLET4.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  int i,j,k;
+  clrscr();
+   for (i=1;i<=5;i++)
+   {
+   for(k=1;k<i;k++)
+  {
+    printf(" ");
+  }
+  for(j=5;j>=i;j--)
+  {
+   (j%2==1)?
+	   printf("1"):printf("0");
+
+
+  }
+  printf("\n");
+  }
+getch();
+}
